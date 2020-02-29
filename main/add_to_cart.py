7e16c2fa1449cd9bd5534d58b1fa06711fc1ecb3
fuhanjie <fuhanjie@yy.com>
2020-02-29 22:13:29 +0800

    fix

--- main/add_to_cart.py
+++ main/add_to_cart.py
@@ -27,7 +27,7 @@ class add_to_cart:
                 self.login()
             else:
                 break
-
+        self.op_click()
         try:
             seconds_delta = (self.config['op_time'] - datetime.now()).seconds
             if seconds_delta > 0:
@@ -39,19 +39,12 @@ class add_to_cart:
         while not self.find_element_by_id('J_LinkBuy'):
             self.browser.refresh()
 
-        if self.config.get('oper'):
-            for oper in self.config['oper'].split(';'):
-                element = self.find_by_link_text(oper)
-                print(element)
-                if not element:
-                    continue
-                self.click(element)
-
         print('start to next op')
         if self.config.get('is_cart', False):
             self.op_cart()
         if self.config.get('is_buy', False):
             while True:
+                self.op_click()
                 flag = self.op_buy()
                 if flag:
                     break
@@ -76,6 +69,15 @@ class add_to_cart:
     def op_cart(self):
         element = self.browser.find_element_by_id('J_LinkBasket')
         self.click(element)
+
+    def op_click(self):
+        if self.config.get('oper'):
+            for oper in self.config['oper'].split(';'):
+                element = self.find_by_link_text(oper)
+                print(element)
+                if not element:
+                    continue
+                self.click(element)
 
     def find_element_by_name(self, name):
         from selenium.common.exceptions import NoSuchElementException
